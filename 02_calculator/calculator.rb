f115a1c13a885415223d8a9925a9b431587ea645
seanrson <seanrichardson@outlook.com>
2020-03-05 21:42:51 -0800

    0-2 comp

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
 #write your code here
+def add(a,b)
+    return a+b
+end
+
+def subtract(a,b)
+    return a-b
+end
+
+def sum(nums)
+    total = 0
+    for n in nums do
+        total += n
+    end
+    return total
+end
+
+def factorial(n)
+    if (n==0) ## base case
+        return 1
+    end
+    return n * factorial(n-1)
+end
